52fca6b7eca784eca29e5cab17c6b75c4ef0e97c
oipi <tuberosumgames@gmail.com>
2024-06-28 13:38:39 +0530

    added express and nodemon

--- lostscript.js
+++ lostscript.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 const http = require('http');
-const server = http.createServer(function(req,res){
-    res.end("hello world");
+const express = require('express')
+const app = express()
+
+app.use(function(req,res,next){
+    console.log("middleware started")
+    next()
 })
 
-server.listen(3000);
+app.get("/", function (req, res) {
+  res.send('Hello World')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+app.listen(3000)
