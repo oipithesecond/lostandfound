f16dbce1f7f881abe80b0cec0f3654290eac080c
Aditya <146020942+adityaxgupta@users.noreply.github.com>
2024-09-16 00:41:17 +0530

    login revamp
    revamped login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,55 +1,55 @@
-console.clear();
+// console.clear();
 
-const loginBtn = document.getElementById('login');
-const signupBtn = document.getElementById('signup');
+// const loginBtn = document.getElementById('login');
+// const signupBtn = document.getElementById('signup');
 
-loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
-});
+// loginBtn.addEventListener('click', (e) => {
+// 	let parent = e.target.parentNode.parentNode;
+// 	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+// 		if(element !== "slide-up") {
+// 			parent.classList.add('slide-up')
+// 		}else{
+// 			signupBtn.parentNode.classList.add('slide-up')
+// 			parent.classList.remove('slide-up')
+// 		}
+// 	});
+// });
 
-signupBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode;
-	Array.from(e.target.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			loginBtn.parentNode.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
-});
+// signupBtn.addEventListener('click', (e) => {
+// 	let parent = e.target.parentNode;
+// 	Array.from(e.target.parentNode.classList).find((element) => {
+// 		if(element !== "slide-up") {
+// 			parent.classList.add('slide-up')
+// 		}else{
+// 			loginBtn.parentNode.parentNode.classList.add('slide-up')
+// 			parent.classList.remove('slide-up')
+// 		}
+// 	});
+// });
 
-document.querySelector('.signup form').addEventListener('submit', function(e) {
-	const nameInput = document.querySelector('.signup form input[name="name"]');
-    const name = nameInput.value;
-    const namePattern = /^[a-zA-Z\s]{3,}$/;
+// document.querySelector('.signup form').addEventListener('submit', function(e) {
+// 	const nameInput = document.querySelector('.signup form input[name="name"]');
+//     const name = nameInput.value;
+//     const namePattern = /^[a-zA-Z\s]{3,}$/;
 
-    if (!namePattern.test(name)) {
-        e.preventDefault(); 
-        alert("Name can only contain letters and spaces and it should be of minimum three letters.");
-    }
-    const emailInput = document.querySelector('.signup form input[name="email"]');
-    const email = emailInput.value;
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@vitapstudent\.ac\.in$/;
+//     if (!namePattern.test(name)) {
+//         e.preventDefault(); 
+//         alert("Name can only contain letters and spaces and it should be of minimum three letters.");
+//     }
+//     const emailInput = document.querySelector('.signup form input[name="email"]');
+//     const email = emailInput.value;
+//     const emailPattern = /^[a-zA-Z0-9._%+-]+@vitapstudent\.ac\.in$/;
 
-    if (!emailPattern.test(email)) {
-        e.preventDefault();  
-        alert("Please enter a valid VIT-AP student email address.");
-    }
-	const passwordInput = document.querySelector('.signup form input[name="password"]');
-    const password = passwordInput.value;
-    const passwordPattern = /^\S{5,}$/; 
+//     if (!emailPattern.test(email)) {
+//         e.preventDefault();  
+//         alert("Please enter a valid VIT-AP student email address.");
+//     }
+// 	const passwordInput = document.querySelector('.signup form input[name="password"]');
+//     const password = passwordInput.value;
+//     const passwordPattern = /^\S{5,}$/; 
 
-    if (!passwordPattern.test(password)) {
-        e.preventDefault();  
-        alert("Password must be at least 5 characters long and cannot contain spaces.");
-    }
-});
+//     if (!passwordPattern.test(password)) {
+//         e.preventDefault();  
+//         alert("Password must be at least 5 characters long and cannot contain spaces.");
+//     }
+// });
