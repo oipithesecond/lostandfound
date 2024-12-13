9e7c5641bd777b6a8d7800f2ff261e29212c1f4d
oipi <tuberosumgames@gmail.com>
2024-12-13 22:25:53 +0530

    Update PostrateLimiter.js

--- middleware/PostrateLimiter.js
+++ middleware/PostrateLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 
 const limiter = rateLimit({
-  windowMs: 30 * 60 * 1000,
+  windowMs: 1 * 60 * 1000,
   max: 1, 
   handler: function (req, res) {
     res.status(429).json({ message: 'Rate limit exceeded' })
